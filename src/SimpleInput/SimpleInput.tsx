d41dc535b523f55025220a4e70d248d606f29d74
Yury Savin <savinyurii@gmail.com>
2019-04-12 21:41:04 +0300

    Move functions out of component scope

--- src/SimpleInput/SimpleInput.tsx
+++ src/SimpleInput/SimpleInput.tsx
@@ -4,43 +4,41 @@ import { saveAs } from "file-saver";
 
 import Papa from "papaparse";
 
-function SimpleInput() {
-  const handleFileRead = (progressEvent: ProgressEvent) => {
-    const fileReader = progressEvent.target as FileReader;
+const handleFileRead = (progressEvent: ProgressEvent) => {
+  const fileReader = progressEvent.target as FileReader;
 
-    if (fileReader) {
-      const content = fileReader.result;
-      if (typeof content === "string") {
-        // console.log(content);
-        const csvObject = Papa.parse(content);
-        console.log(csvObject);
-        var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
-        // saveAs(blob, "export.csv");
-      } else {
-        console.error("File is empty");
-      }
+  if (fileReader) {
+    const content = fileReader.result;
+    if (typeof content === "string") {
+      // console.log(content);
+      const csvObject = Papa.parse(content);
+      console.log(csvObject);
+      var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+      // saveAs(blob, "export.csv");
+    } else {
+      console.error("File is empty");
     }
-  };
+  }
+};
 
-  const handleFileChosen = (file: Blob) => {
-    const fileReader = new FileReader();
-    fileReader.onloadend = handleFileRead;
-    fileReader.readAsText(file);
-  };
+const handleFileChosen = (file: Blob) => {
+  const fileReader = new FileReader();
+  fileReader.onloadend = handleFileRead;
+  fileReader.readAsText(file);
+};
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log("handleInputChange", e);
-    if (e && e.target && e.target.files) {
-      const file = e.target.files[0];
-      handleFileChosen(file);
-    }
-  };
+const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // console.log("handleInputChange", e);
+  if (e && e.target && e.target.files) {
+    const file = e.target.files[0];
+    handleFileChosen(file);
+  }
+};
 
+export default function SimpleInput() {
   return (
     <>
       <input type="file" accept=".csv" onChange={handleInputChange} />
     </>
   );
 }
-
-export default SimpleInput;
